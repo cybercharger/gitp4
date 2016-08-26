7dc59ec41cab39b2a63c24947026708085aea7a4
chriskang <chriskang@ea.com>
2016-08-26 12:33:51 +0800

    p4 clone & p4 sync

---
+++ src/main/java/gitp4/git/cmd/GitCheckout.java
@@ -0,0 +1,7 @@
+package gitp4.git.cmd;
+
+/**
+ * Created by chriskang on 8/26/2016.
+ */
+public class GitCheckout {
+}
---
+++ src/test/java/gitp4/GitP4BridgeTest.java
@@ -0,0 +1,7 @@
+package gitp4;
+
+/**
+ * Created by chriskang on 8/26/2016.
+ */
+public class GitP4BridgeTest {
+}
