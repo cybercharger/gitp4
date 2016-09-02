0e211bc364ddbecdd7f7cae1c599321e8f4c6ed8
chriskang <chriskang@ea.com>
2016-09-02 12:26:53 +0800

    sync @=changlist on p4 client and then copy & git add/rm, multithread copy & git add is correct

---
+++ src/main/java/gitp4/p4/P4FileInfoEx.java
@@ -0,0 +1,7 @@
+package gitp4.p4;
+
+/**
+ * Created by chriskang on 9/1/2016.
+ */
+public class P4FileInfoEx {
+}
---
+++ src/main/java/gitp4/p4/cmd/P4Sync.java
@@ -0,0 +1,7 @@
+package gitp4.p4.cmd;
+
+/**
+ * Created by chriskang on 8/31/2016.
+ */
+public class P4Sync {
+}
